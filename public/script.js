a053ec96ab98674faea739bdf52a2faa8e6c2817
Peetz0r <github@haas-en-berg.nl>
2021-04-05 05:34:52 +0200

    EXIF metadata display

--- public/script.js
+++ public/script.js
@@ -15,3 +15,46 @@ document.onkeydown = function(e) {
       break;
   }
 }
+
+window.addEventListener('load', function() {
+  const im = document.querySelector('img.large')
+  const dl = document.querySelector('dl');
+  if (im) {
+    EXIF.getData(im, function() {
+      const exif = EXIF.getAllTags(this);
+      console.log(exif);
+      if (Object.keys(exif).length < 1) {
+        dl.remove();
+        return;
+      }
+
+      let d = {};
+      if(exif.Model) d[exif.Make] = exif.Model;
+      if(exif.FocalLength) d['Focal Length'] = exif.FocalLength+' mm';
+      if(exif.FocalLengthIn35mmFilm) d['35mm equivalent'] = exif.FocalLengthIn35mmFilm+' mm';
+      if(exif.FNumber) d['Aperture'] ='f/'+exif.FNumber;
+      if(exif.ExposureTime) d['Exposure time'] = (exif.ExposureTime <1 ? ('1/' + 1/exif.ExposureTime) : exif.ExposureTime) + ' s';
+      if(exif.DateTime) d['Date/Time'] = exif.DateTime;
+      if(exif.ISOSpeedRatings) d['ISO'] = exif.ISOSpeedRatings;
+      if(exif.ExposureBias != undefined) d['EV'] = (exif.ExposureBias>0?'+':'') + exif.ExposureBias;
+      if(exif.MeteringMode) d['Metering Mode'] = exif.MeteringMode;
+      if(exif.ExposureProgram) d['Exposure Program'] = exif.ExposureProgram;
+      if(exif.WhiteBalance) d['White Balance'] = exif.WhiteBalance;
+      if(exif.Software) d['Software'] = exif.Software;
+
+      for (const i in d) {
+        let dt = document.createElement('dt');
+        let dd = document.createElement('dd');
+        dt.innerText = i;
+        dd.innerText = d[i];
+        dl.appendChild(dt);
+        dl.appendChild(dd);
+      }
+
+      dl.addEventListener('wheel', function(e) {
+        if (e.deltaY > 0) dl.scrollLeft += 50;
+        else dl.scrollLeft -= 50;
+      });
+    });
+  }
+});
