c189ce42d97b89f424542623105eb1a09b87dfbf
Peetz0r <github@haas-en-berg.nl>
2021-04-08 01:53:24 +0200

    Fixed number formatting
    No more "1/1.2999999999999998 s" exposures
    
    Also a bunch of whitespace changes at the same time. Sorry.

--- public/script.js
+++ public/script.js
@@ -19,6 +19,8 @@ document.onkeydown = function(e) {
 window.addEventListener('load', function() {
   const im = document.querySelector('img.large')
   const dl = document.querySelector('dl');
+  const f = new Intl.NumberFormat('en-US');
+
   if (im) {
     EXIF.getData(im, function() {
       const exif = EXIF.getAllTags(this);
@@ -29,18 +31,49 @@ window.addEventListener('load', function() {
       }
 
       let d = {};
-      if(exif.Model) d[exif.Make] = exif.Model;
-      if(exif.FocalLength) d['Focal Length'] = exif.FocalLength+' mm';
-      if(exif.FocalLengthIn35mmFilm) d['35mm equivalent'] = exif.FocalLengthIn35mmFilm+' mm';
-      if(exif.FNumber) d['Aperture'] ='f/'+exif.FNumber;
-      if(exif.ExposureTime) d['Exposure time'] = (exif.ExposureTime <1 ? ('1/' + 1/exif.ExposureTime) : exif.ExposureTime) + ' s';
-      if(exif.DateTime) d['Date/Time'] = exif.DateTime;
-      if(exif.ISOSpeedRatings) d['ISO'] = exif.ISOSpeedRatings;
-      if(exif.ExposureBias != undefined) d['EV'] = (exif.ExposureBias>0?'+':'') + exif.ExposureBias;
-      if(exif.MeteringMode) d['Metering Mode'] = exif.MeteringMode;
-      if(exif.ExposureProgram) d['Exposure Program'] = exif.ExposureProgram;
-      if(exif.WhiteBalance) d['White Balance'] = exif.WhiteBalance;
-      if(exif.Software) d['Software'] = exif.Software;
+      if(exif.Model) {
+        d[exif.Make] = exif.Model;
+      }
+      if(exif.FocalLength) {
+        d['Focal Length'] = exif.FocalLength+' mm';
+      }
+      if(exif.FocalLengthIn35mmFilm) {
+        d['35mm equivalent'] = exif.FocalLengthIn35mmFilm+' mm';
+      }
+      if(exif.FNumber) {
+        d['Aperture'] ='f/'+exif.FNumber;
+      }
+      if(exif.ExposureTime) {
+        if(exif.ExposureTime <1) {
+          d['Exposure time'] = '1/' + f.format(1/exif.ExposureTime) + ' s';
+        } else {
+          d['Exposure time'] = f.format(exif.ExposureTime) + ' s';
+        }
+      }
+      if(exif.ISOSpeedRatings) {
+        d['ISO'] = exif.ISOSpeedRatings;
+      }
+      if(exif.ExposureBias != undefined) {
+        d['EV'] = (exif.ExposureBias>0?'+':'') + exif.ExposureBias;
+      }
+      if(exif.MeteringMode) {
+        d['Metering Mode'] = exif.MeteringMode;
+      }
+      if(exif.ExposureProgram) {
+        d['Exposure Program'] = exif.ExposureProgram;
+      }
+      if(exif.WhiteBalance) {
+        d['White Balance'] = exif.WhiteBalance;
+      }
+      if(exif.Software) {
+        d['Software'] = exif.Software;
+      }
+      if(exif.DateTime) {
+        d['Date/Time'] = exif.DateTime.replace(/(\d{4}):(\d{2}):/, '$1-$2-')
+      }
+      if(exif.DateTime && exif.SubsecTime) {
+        d['Date/Time'] += '.' + String(exif.SubsecTime).padStart(3, '0');
+      }
 
       for (const i in d) {
         let dt = document.createElement('dt');
